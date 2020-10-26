f7f534a0ad1a06a906f2260a49296fbd3f6d5417
patjawat <patjawat@gmail.com>
2020-10-26 19:48:40 +0700

    update

--- Booking-reactapp/src/pages/booking/BookingForm.js
+++ Booking-reactapp/src/pages/booking/BookingForm.js
@@ -1,66 +1,93 @@
-import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
+import React from "react";
 import { useForm, Controller } from "react-hook-form";
 import { inputField, SelectField, SwitchField } from "../../components/Inputs";
-import { Button, Card } from "antd";
-import { CheckOutlined } from '@ant-design/icons';
-import Axios from '../../axios.config'
+import { Button } from "antd";
 
-export default function BookingForm() {
-    const items = useSelector(state => state.meettingroom.addBooking);
-    const { handleSubmit, control, errors, reset } = useForm();
-    const type = ["Student", "Developer", "other"];
+function Login(props) {
+	const { handleSubmit, control, errors, reset } = useForm();
+	const type = ["Student", "Developer", "other"];
 
-    const onSubmit = async (data) => {
-        let res = await Axios.post('category', {
-            name: data.name,
-            type: 'department'
-        });
-        await reset({ name: '', type: '' })
-        // await setCategory([...items, res.data.item]);
-    };
-    return (
-        <div>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <div className="row">
-                    <div className="col-6">
-                        <div className='input-group'>
-                            <label className='label'>เรื่อง/หัวข้อ</label>
-                            <Controller
-                                as={inputField("name")}
-                                name='topic'
-                                control={control}
-                                defaultValue=''
-                                rules={{ required: true }}
-                            />
+	const onSubmit = (data) => {
+		console.log(data)
+		setTimeout(() => reset({
+			FirstName:'',
+			LastName:'',
+			Email: '',
+		}), 1000);
 
-                            {errors.name && (
-                                <span className='error'>This field is required</span>
-                            )}
-                        </div>
-       
-                        <div className='input-group'>
-                            <p className="mb-5">
-
-                            <label className='label'>Type of user</label>
-                            </p>
-                            <Controller
-                                as={SelectField(type[0], type)}
-                                name='type'
-                                control={control}
-                                defaultValue={type[0]}
-                                rules={{
-                                    required: true,
-                                }}
-                            />
-                        </div>
-                    </div>
-                </div>
-                <br />
-                <br />
-                <br />
-                <Button type='primary' htmlType='submit' ><CheckOutlined />บันทึก</Button>
-            </form>
-        </div>
-    )
+	};
+	return (
+		<form onSubmit={handleSubmit(onSubmit)}>
+			<div className='input-group'>
+				<label className='label'>Firstname</label>
+				<Controller
+					as={inputField("FirstName")}
+					name='FirstName'
+					control={control}
+					defaultValue=''
+					rules={{ required: true }}
+				/>
+				{errors.FirstName && (
+					<span className='error'>This field is required</span>
+				)}
+			</div>
+			<div className='input-group'>
+				<label className='label'>LastName</label>
+				<Controller
+					as={inputField("LastName")}
+					name='LastName'
+					control={control}
+					defaultValue=''
+					rules={{ required: true }}
+				/>
+				{errors.LastName && (
+					<span className='error'>This field is required</span>
+				)}
+			</div>
+			<div className='input-group'>
+				<label className='label'>Email</label>
+				<Controller
+					as={inputField("Email")}
+					name='Email'
+					control={control}
+					defaultValue=''
+					rules={{
+						pattern: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+						required: true,
+					}}
+				/>
+				{errors.Email && (
+					<span className='error'>Please add a valid email</span>
+				)}
+			</div>
+			<div className='input-group'>
+				<label className='label'>Type of user</label>
+				<Controller
+					as={SelectField(type[0], type)}
+					name='type'
+					control={control}
+					defaultValue={type[0]}
+					rules={{
+						required: true,
+					}}
+				/>
+			</div>
+			<div className='input-group'>
+				<label className='label'>
+					Want to suscribe to our journal?
+				</label>
+				<Controller
+					as={SwitchField()}
+					name='subscription'
+					control={control}
+					defaultValue={true}
+				/>
+			</div>
+			<Button type='primary' htmlType='submit'>
+				Register
+			</Button>
+		</form>
+	);
 }
+
+export default Login;
